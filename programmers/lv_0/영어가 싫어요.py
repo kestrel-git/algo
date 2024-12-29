44423ef8ce50d91d8e607bf459f6928b4e4939ca
kestrel-git <parusvarius.b@gmail.com>
2024-12-30 00:00:56 +0900

    Add

---
+++ programmers/lv_0/영어가 싫어요.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    n_dict = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
+              'six': '6', 'seven': '7', 'eight': '8', 'nine': '9', 'zero': '0'}
+    for key, val in n_dict.items():
+        numbers = numbers.replace(key, val)
+    return int(numbers)
