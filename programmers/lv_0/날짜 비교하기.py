f9d5c4ec40ad7b89fa72352fc1b499bd94b47fce
kestrel-git <parusvarius.b@gmail.com>
2025-01-09 00:30:44 +0900

    Add

---
+++ programmers/lv_0/날짜 비교하기.py
@@ -0,0 +1,9 @@
+def solution(date1, date2):
+    # for idx in range(3):
+    #     if date1[idx] < date2[idx]:
+    #         return 1
+    #     elif date1[idx] > date2[idx]:
+    #         break        
+    # return 0
+    
+    return int(date1 < date2)
