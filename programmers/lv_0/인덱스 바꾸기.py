40d702a086145012e6ab028d6f9f702f02f46618
kestrel-git <parusvarius.b@gmail.com>
2025-02-03 03:28:28 +0900

    Add

---
+++ programmers/lv_0/인덱스 바꾸기.py
@@ -0,0 +1,4 @@
+def solution(my_string, num1, num2):
+    list_string = list(my_string)
+    list_string[num1], list_string[num2] = list_string[num2], list_string[num1]
+    return ''.join(list_string)
